9bf15b6948aaeb9a81f3215f7b2da87ca4e7a09a
xyz999673 <1447955562@qq.com>
2021-01-29 17:11:15 +0800

    b

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author shkstart
+ * @create 2021-01-29 17:07
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
